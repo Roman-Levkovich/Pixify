6e500596de6354af70382ba85522aec3ec726bad
Roman-Levkovich <roman_levkovich@mail.ru>
2025-06-13 16:14:21 +0300

    Initial Commit

---
+++ Pixify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pixify
+//
+//  Created by Roman Levkovich on 13.06.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!fdfgdfgfdggdfgd")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pixify/PixifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixifyApp.swift
+//  Pixify
+//
+//  Created by Roman Levkovich on 13.06.25.
+//
+
+import SwiftUI
+
+@main
+struct PixifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
